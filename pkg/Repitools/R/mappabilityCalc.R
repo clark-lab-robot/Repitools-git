70397df2e4c2cf19fd4759cd9d7ce6c7fe8e80ec
markrobinsonca <markrobinsonca@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-12 11:23:03 +0000

    change mappabilityCalc() to return mappability, instead of non-mappability
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/repitools@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Repitools/R/mappabilityCalc.R
+++ pkg/Repitools/R/mappabilityCalc.R
@@ -1,3 +1,3 @@
 mappabilityCalc <- function(locations, window = 500, organism, verbose=FALSE, chunkSize=10000000) {	
-	return(sequenceCalc(locations, window, organism, DNAString("N"), verbose=verbose, chunkSize=chunkSize)/window)
+	return(1-sequenceCalc(locations, window, organism, DNAString("N"), verbose=verbose, chunkSize=chunkSize)/window)
 }
