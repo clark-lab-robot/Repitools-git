828610e7fe0c72bd9646c62adf97b7500685ea66
aarsta <aarsta@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-15 07:02:06 +0000

    Added enrichmentCalc & enrichmentPlot
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/repitools@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/Repitools/R/enrichmentPlot.R
@@ -0,0 +1,21 @@
+setMethodS3("enrichmentPlot", "GenomeDataList", function(rs, organism, seqLen, cols=rainbow(length(rs)), xlim=c(0,20), main="Enrichment Plot", total.lib.size=TRUE, verbose=FALSE, ...) {
+	if (length(cols)!=length(rs)) stop("rs and cols must have the same number of elements.")
+	if (verbose) cat("Extending reads\n")
+	rs.ext <- chipseq::extendReads(rs, seqLen=seqLen)
+	if (verbose) cat("Creating coverage object\n")
+	rs.cov <- BSgenome::gdapply(rs.ext, IRanges::coverage)
+	if (verbose) cat("Calculating enrichment\n")
+	rs.enrich <- enrichmentCalc(rs.cov, organism, ...)
+	if (total.lib.size) {
+		if (verbose) cat("Normalising to reads per lane\n")
+		rs.counts <- laneCounts(rs)
+		for (i in 1:length(rs)) rs.enrich[[i]]$coverage <- rs.enrich[[i]]$coverage/(rs.counts[[i]]/1000000)
+	}
+	plot(x=rs.enrich[[1]]$coverage, y=rs.enrich[[1]]$bases, type="l", col=cols[1], xlim=xlim, main=main, ylab="Frequency", log="y",
+			xlab=if (total.lib.size) "Normalised Enrichment Level of reads" else "Enrichment Level of reads")
+	if (length(rs)>1) for (i in 2:length(rs)) {
+		lines(x=rs.enrich[[i]]$coverage, y=rs.enrich[[i]]$bases, col=cols[i])
+	}
+	legend("topright", lty=1, col=cols, legend=names(rs))
+	invisible(rs.enrich)
+})
