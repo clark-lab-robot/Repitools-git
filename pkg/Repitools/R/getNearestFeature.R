40ad5083c19adab480af233b6c28c8f8864b0084
dario <dario@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-20 05:17:45 +0000

    getNearestFeature function and documentation is added.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/repitools@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/Repitools/R/getNearestFeature.R
@@ -0,0 +1,30 @@
+getNearestFeature <- function(peaksTable, featuresTable)
+{
+	require(ChIPpeakAnno)
+	
+	if("strand" %in% colnames(peaksTable))
+	{
+		peaksRangedData <- RangedData(space = peaksTable$chr, strand = peaksTable$strand, ranges = IRanges(start = peaksTable$start, end = peaksTable$end))
+	} else {
+		peaksRangedData <- RangedData(space = peaksTable$chr, ranges = IRanges(start = peaksTable$start, end = peaksTable$end))
+	}
+
+	if("strand" %in% colnames(featuresTable))
+	{
+		featuresRangedData <- RangedData(name = featuresTable$name, space = featuresTable$chr, strand = featuresTable$strand, ranges = IRanges(start = featuresTable$start, end = featuresTable$end))
+		featureLoc <- "TSS"
+	} else {
+		featuresRangedData <- RangedData(name = featuresTable$name, space = featuresTable$chr, ranges = IRanges(start = featuresTable$start, end = featuresTable$end))
+		featureLoc <- "middle"
+	}	
+	
+	annoPeaks <- as.data.frame(annotatePeakInBatch(peaksRangedData, AnnotationData = featuresRangedData, PeakLocForDistance = "middle"))
+	annoPeaks <- annoPeaks[order(annoPeaks$peak), c("feature", "space", "strand", "start_position", "end_position", "insideFeature", "distancetoFeature")]
+	colnames(annoPeaks)[which(colnames(annoPeaks) == "insideFeature")] <- "peakLocation"
+	colnames(annoPeaks)[which(colnames(annoPeaks) == "distancetoFeature")] <- "distance"
+	colnames(annoPeaks)[which(colnames(annoPeaks) == "start_position")] <- "featureStart"
+	colnames(annoPeaks)[which(colnames(annoPeaks) == "end_position")] <- "featureEnd"
+	annoPeaks$feature <- featuresTable[annoPeaks$feature, "name"]
+	annoPeaks <- annoPeaks[, -2]
+	return(annoPeaks)
+}
