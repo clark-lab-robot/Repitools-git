56136c310208b2149be02511782a8b0e463dec05
dario <dario@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-31 04:17:34 +0000

    Nearest feature resorting bug fixed.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/repitools@34 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Repitools/R/getNearestFeature.R
+++ pkg/Repitools/R/getNearestFeature.R
@@ -1,15 +1,24 @@
 getNearestFeature <- function(peaksTable, featuresTable)
 {
 	require(ChIPpeakAnno)
-	if(packageDescription("ChIPpeakAnno")$Version < "1.5.3")
-		warning("Your version of ChIPpeakAnno is not 1.5.3 or greater. Versions before this had bugs in them.")
 	
 	if("strand" %in% colnames(peaksTable))
 	{
 		peaksRangedData <- RangedData(space = peaksTable$chr, strand = peaksTable$strand, ranges = IRanges(start = peaksTable$start, end = peaksTable$end))
 	} else {
-		peaksRangedData <- RangedData(space = peaksTable$chr, ranges = IRanges(start = peaksTable$start, end = peaksTable$end))
+		if("name" %in% colnames(peaksTable)) # Maybe the peaks are CpG islands from a database, and do have a name.
+		{
+			peaksRangedData <- RangedData(name = peaksTable$name, space = peaksTable$chr, ranges = IRanges(start = peaksTable$start, end = peaksTable$end))
+		} else {
+			peaksRangedData <- RangedData(space = peaksTable$chr, ranges = IRanges(start = peaksTable$start, end = peaksTable$end))
+		}
 	}
+
+	RDspaces <- space(peaksRangedData)
+	RDstarts <- start(peaksRangedData)
+	RDends <- end(peaksRangedData)
+	peaksRDtoTableMap <- mapply(function(chr, start, end) which(RDspaces == chr & RDstarts == start & RDends == end), 
+                                   peaksTable$chr, peaksTable$start, peaksTable$end)
 
 	if("strand" %in% colnames(featuresTable))
 	{
@@ -21,12 +30,13 @@ getNearestFeature <- function(peaksTable, featuresTable)
 	}	
 	
 	annoPeaks <- as.data.frame(annotatePeakInBatch(peaksRangedData, AnnotationData = featuresRangedData, PeakLocForDistance = "middle"))
-	annoPeaks <- annoPeaks[order(annoPeaks$peak), c("feature", "space", "strand", "start_position", "end_position", "insideFeature", "distancetoFeature")]
+	annoPeaks$feature <- unlist(values(featuresRangedData)[, "name"])[as.numeric(annoPeaks$feature)]
+	annoPeaks <- annoPeaks[order(as.numeric(annoPeaks$peak)), ]
+	annoPeaks <- annoPeaks[peaksRDtoTableMap, ]
 	colnames(annoPeaks)[which(colnames(annoPeaks) == "insideFeature")] <- "peakLocation"
 	colnames(annoPeaks)[which(colnames(annoPeaks) == "distancetoFeature")] <- "distance"
 	colnames(annoPeaks)[which(colnames(annoPeaks) == "start_position")] <- "featureStart"
 	colnames(annoPeaks)[which(colnames(annoPeaks) == "end_position")] <- "featureEnd"
-	annoPeaks$feature <- featuresTable[annoPeaks$feature, "name"]
 	annoPeaks <- annoPeaks[, -2]
 	return(annoPeaks)
 }
