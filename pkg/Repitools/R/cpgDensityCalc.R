c16d80303b95c86ef83e3271280fab2a67f295c0
aarsta <aarsta@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-03 10:09:46 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/repitools@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Repitools/R/cpgDensityCalc.R
+++ pkg/Repitools/R/cpgDensityCalc.R
@@ -1,41 +1,38 @@
 setMethodS3("cpgDensityCalc", "GenomeDataList", function(rs, seqLen, ...) {
-	require(BSgenome.Hsapiens.UCSC.hg18)
-	return(lapply(IRanges::as.list(rs), cpgDensityCalc, seqLen, ...))
-})
+            return(lapply(IRanges::as.list(rs), cpgDensityCalc, seqLen, ...))
+        })
 
 
 setMethodS3("cpgDensityCalc", "GenomeData", function(rs, seqLen, ...) {
-	require(BSgenome.Hsapiens.UCSC.hg18)
-	rs.midpt <- vector(mode='list', length=length(rs))
-	names(rs.midpt) <- names(rs)
-	
-	for (chr in names(rs)) if (length(rs[[chr]][["+"]])+length(rs[[chr]][["+"]])>0) {
-		rs.midpt[[chr]] <- data.frame(chr=chr, position=c(rs[[chr]][["+"]]+seqLen/2, rs[[chr]][["-"]]-seqLen/2), stringsAsFactors=FALSE)
-	}
-	rs.midpt <- do.call(rbind, rs.midpt)
-	
-	return(cpgDensityCalc(rs.midpt, window=seqLen, ...))
-			
-})
+            rs.midpt <- vector(mode='list', length=length(rs))
+            names(rs.midpt) <- names(rs)
+            
+            for (chr in names(rs)) if (length(rs[[chr]][["+"]])+length(rs[[chr]][["+"]])>0) {
+                    rs.midpt[[chr]] <- data.frame(chr=chr, position=c(rs[[chr]][["+"]]+seqLen/2, rs[[chr]][["-"]]-seqLen/2), stringsAsFactors=FALSE)
+                }
+            rs.midpt <- do.call(rbind, rs.midpt)
+            
+            return(cpgDensityCalc(rs.midpt, window=seqLen, ...))
+            
+        })
 
 setMethodS3("cpgDensityCalc", "data.frame", function(locations, window=500, wFunction=c("linear","exp","log","none"), organism, verbose=FALSE, chunkSize=10000000, ...) {
-	require(BSgenome.Hsapiens.UCSC.hg18)
-	wFunction <- match.arg(wFunction)
-
-	if(wFunction == "none") {
-		cpgDensity <- sequenceCalc(locations, window, organism, DNAString("CG"), verbose=verbose, chunkSize=chunkSize)
-	} else {
-		CGfinds <- sequenceCalc(locations, window, organism, DNAString("CG"), verbose=verbose, positions=TRUE)
-		distances <- lapply(CGfinds, function(positionsInRegion) {if(!is.null(positionsInRegion)) abs(positionsInRegion)})
-		if(wFunction == "linear") {
-			cpgDensity <- sapply(distances, function(distancesInRegion) sum(1 - (distancesInRegion / (window / 2))))
-		} else if(wFunction == "log") {
-			cpgDensity <- sapply(distances, function(distancesInRegion) sum(log2(2 - (distancesInRegion / (window / 2)))))
-		} else { # Exponential decay was requested.
-			cpgDensity <- sapply(distances, function(distancesInRegion) sum(exp(-5 * distancesInRegion / (window / 2))))	
-		}
-		rm(CGfinds)
-	}
-	gc()
-	return(cpgDensity)
-})
+            wFunction <- match.arg(wFunction)
+            
+            if(wFunction == "none") {
+                cpgDensity <- sequenceCalc(locations, window, organism, DNAString("CG"), verbose=verbose, chunkSize=chunkSize)
+            } else {
+                CGfinds <- sequenceCalc(locations, window, organism, DNAString("CG"), verbose=verbose, positions=TRUE)
+                distances <- lapply(CGfinds, function(positionsInRegion) {if(!is.null(positionsInRegion)) abs(positionsInRegion)})
+                if(wFunction == "linear") {
+                    cpgDensity <- sapply(distances, function(distancesInRegion) sum(1 - (distancesInRegion / (window / 2))))
+                } else if(wFunction == "log") {
+                    cpgDensity <- sapply(distances, function(distancesInRegion) sum(log2(2 - (distancesInRegion / (window / 2)))))
+                } else { # Exponential decay was requested.
+                    cpgDensity <- sapply(distances, function(distancesInRegion) sum(exp(-5 * distancesInRegion / (window / 2))))	
+                }
+                rm(CGfinds)
+            }
+            gc()
+            return(cpgDensity)
+        })
