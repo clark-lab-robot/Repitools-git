1bb4de586577770f608fc346dfd5345bd237ccc5
dario <dario@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-07-19 01:50:18 +0000

    checkProbes documentation uses different example. Midpoint of probe hit used as probe position now.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/repitools@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Repitools/R/checkProbes.R
+++ pkg/Repitools/R/checkProbes.R
@@ -1,5 +1,6 @@
-checkProbes <- function(regionsTable, probesTable)
+checkProbes <- function(regionsTable, probesTable, outFile = "Probe Specificity")
 {
+	probesTable$pos <- round((probesTable$start + probesTable$end) / 2 )
 	hits <- as.data.frame(table(probesTable$name), stringsAsFactors = FALSE)
 	colnames(hits) <- c("name", "number")
 	regions <- GRanges(seqnames = regionsTable$chr, ranges = IRanges(start = regionsTable$start, end = regionsTable$end))
@@ -7,11 +8,11 @@ checkProbes <- function(regionsTable, probesTable)
 	overlaps <- findOverlaps(regions, probes)@matchMatrix
 	indsPerRegion <- split(overlaps[, 2], overlaps[, 1])
 
-	pdf("Probe Specificity.pdf", h = 8, w = 12)
+	pdf(paste(outFile, ".pdf", sep = ""), h = 8, w = 12)
 	invisible(mapply(function(region, probesInRegion) {
 		par(oma=c(4, 1, 1, 1))
 		hitsMatch <- match(probesTable[probesInRegion, "name"], hits[, "name"])
-		plot(x = probesTable[probesInRegion, "start"], y = hits[hitsMatch, "number"], xlim = c(regionsTable[region, "start"], regionsTable[region, "end"]), yaxt="n", type = 'p', pch = 19, cex = 0.5, main = regionsTable[region, "name"], xlab = "Position", ylab = "Probe Hits", col = c("red", "green", "blue"))
+		plot(x = probesTable[probesInRegion, "pos"], y = hits[hitsMatch, "number"], xlim = c(regionsTable[region, "start"], regionsTable[region, "end"]), yaxt="n", type = 'p', pch = 19, cex = 0.5, main = regionsTable[region, "name"], xlab = "Position", ylab = "Probe Hits", col = c("red", "green", "blue"))
 		ymax = max(hits[hitsMatch, "number"])
 		if(ymax > 100)
 		{
