01aa8689f0a451538e2f153730ca670243bfd88b
dario <dario@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-09 01:29:42 +0000

    enrichmentCalc doesn't need chrs specified anymore. Gets chrs from the GRanges object automatically.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/repitools@54 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Repitools/R/enrichmentCalc.R
+++ pkg/Repitools/R/enrichmentCalc.R
@@ -1,9 +1,9 @@
-enrichmentCalc <- function(rs, organism, chrs, seqLen=NULL) {
+enrichmentCalc <- function(rs, organism, seqLen=NULL) {
 	require(GenomicRanges)
 
 	if (class(rs) != "GRanges")
 		stop("rs must be a GRanges object.")
-
+	chrs <- levels(seqnames(rs))
 	seqlengths(rs) <- seqlengths(organism)[chrs]
 	coverage <- colSums(table(coverage(resize(rs, seqLen))))
 	coverageTable <- cbind(as.numeric(names(coverage)), coverage)
