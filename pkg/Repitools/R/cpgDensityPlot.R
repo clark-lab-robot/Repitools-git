5e7dd802f9451d25837a5b65ffa71b127bc96773
dario <dario@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-18 01:29:24 +0000

    cpgDensityPlot and enrichmentPlot now accept lty lwd parameters. User manual updated.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/repitools@75 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Repitools/R/cpgDensityPlot.R
+++ pkg/Repitools/R/cpgDensityPlot.R
@@ -1,27 +1,27 @@
 setGeneric("cpgDensityPlot", function(rs, ...){standardGeneric("cpgDensityPlot")})
 
-setMethod("cpgDensityPlot", "GenomeDataList", function(rs, cols=rainbow(length(rs)), xlim=c(0,20), main="CpG Density Plot", ...) {
+setMethod("cpgDensityPlot", "GenomeDataList", function(rs, cols=rainbow(length(rs)), xlim=c(0,20), lty = 1, lwd = 1, main="CpG Density Plot", ...) {
 	if (length(cols)!=length(rs)) stop("rs and cols must have the same number of elements.")
 	rs.cpg <- lapply(rs, cpgDensityCalc, ...)
 	rs.den <- lapply(rs.cpg, density)
 	ymax <- max(sapply(rs.den, function(u) max(u$y)))
-	plot(x=rs.den[[1]]$x, y=rs.den[[1]]$y, type="l", col=cols[1], xlim=xlim, ylim=c(0,ymax), main=main, ylab="Frequency", xlab="CpG Density of reads")
+	plot(x=rs.den[[1]]$x, y=rs.den[[1]]$y, type="l", col=cols[1], xlim=xlim, ylim=c(0,ymax), main=main, ylab="Frequency", xlab="CpG Density of reads", lty = lty, lwd = lwd)
 	if (length(rs)>1) for (i in 2:length(rs)) {
-		lines(x=rs.den[[i]]$x, y=rs.den[[i]]$y, col=cols[i])
+		lines(x=rs.den[[i]]$x, y=rs.den[[i]]$y, col=cols[i], lty = lty, lwd = lwd)
 	}
-	legend("topright", lty=1, col=cols, legend=names(rs))
+	legend("topright", col=cols, legend=names(rs), lty = lty, lwd = lwd)
 	invisible(rs.cpg)
 })
 
-setMethod("cpgDensityPlot", "GRangesList", function(rs, cols=rainbow(length(rs)), xlim=c(0,20), main="CpG Density Plot", ...) {
+setMethod("cpgDensityPlot", "GRangesList", function(rs, cols=rainbow(length(rs)), xlim=c(0,20), lty = 1, lwd = 1, main="CpG Density Plot", ...) {
 	if (length(cols)!=length(rs)) stop("rs and cols must have the same number of elements.")
 	rs.cpg <- lapply(rs, cpgDensityCalc, ...)
 	rs.den <- lapply(rs.cpg, density)
 	ymax <- max(sapply(rs.den, function(u) max(u$y)))
-	plot(x=rs.den[[1]]$x, y=rs.den[[1]]$y, type="l", col=cols[1], xlim=xlim, ylim=c(0,ymax), main=main, ylab="Frequency", xlab="CpG Density of reads")
+	plot(x=rs.den[[1]]$x, y=rs.den[[1]]$y, type="l", col=cols[1], xlim=xlim, ylim=c(0,ymax), main=main, ylab="Frequency", xlab="CpG Density of reads", lty = lty, lwd = lwd)
 	if (length(rs)>1) for (i in 2:length(rs)) {
-		lines(x=rs.den[[i]]$x, y=rs.den[[i]]$y, col=cols[i])
+		lines(x=rs.den[[i]]$x, y=rs.den[[i]]$y, col=cols[i], lty = lty, lwd = lwd)
 	}
-	legend("topright", lty=1, col=cols, legend=names(rs))
+	legend("topright", col=cols, legend=names(rs), lty = lty, lwd = lwd)
 	invisible(rs.cpg)
 })
--- pkg/Repitools/R/enrichmentPlot.R
+++ pkg/Repitools/R/enrichmentPlot.R
@@ -1,6 +1,6 @@
 setGeneric("enrichmentPlot", function(rs, organism, ...){standardGeneric("enrichmentPlot")})
 
-setMethod("enrichmentPlot", c("GenomeDataList", "BSgenome"), function(rs, organism, seqLen, cols=rainbow(length(rs)), xlim=c(0,20), main="Enrichment Plot", total.lib.size=TRUE, verbose=FALSE) {
+setMethod("enrichmentPlot", c("GenomeDataList", "BSgenome"), function(rs, organism, seqLen, cols=rainbow(length(rs)), xlim=c(0,20), main="Enrichment Plot", total.lib.size=TRUE, verbose=FALSE, ...) {
 	if (length(cols)!=length(rs)) stop("rs and cols must have the same number of elements.")
 	if (verbose) cat("Calculating enrichment\n")
 	rs.enrich <- lapply(rs, enrichmentCalc, organism, seqLen)
@@ -10,15 +10,15 @@ setMethod("enrichmentPlot", c("GenomeDataList", "BSgenome"), function(rs, organi
 		for (i in 1:length(rs)) rs.enrich[[i]]$coverage <- rs.enrich[[i]]$coverage/(rs.counts[[i]]/1000000)
 	}
 	plot(x=rs.enrich[[1]]$coverage, y=rs.enrich[[1]]$bases, type="l", col=cols[1], xlim=xlim, main=main, ylab="Frequency", log="y",
-			xlab=if (total.lib.size) "Normalised Enrichment Level of reads" else "Enrichment Level of reads")
+			xlab=if (total.lib.size) "Normalised Enrichment Level of reads" else "Enrichment Level of reads", ...)
 	if (length(rs)>1) for (i in 2:length(rs)) {
-		lines(x=rs.enrich[[i]]$coverage, y=rs.enrich[[i]]$bases, col=cols[i])
+		lines(x=rs.enrich[[i]]$coverage, y=rs.enrich[[i]]$bases, col=cols[i], ...)
 	}
-	legend("topright", lty=1, col=cols, legend=names(rs))
+	legend("topright", lty=1, col=cols, legend=names(rs), ...)
 	invisible(rs.enrich)
 })
 
-setMethod("enrichmentPlot", c("GRangesList", "BSgenome"), function(rs, organism, seqLen, cols=rainbow(length(rs)), xlim=c(0,20), main="Enrichment Plot", total.lib.size=TRUE, verbose=FALSE) {
+setMethod("enrichmentPlot", c("GRangesList", "BSgenome"), function(rs, organism, seqLen, cols=rainbow(length(rs)), xlim=c(0,20), main="Enrichment Plot", total.lib.size=TRUE, verbose=FALSE, ...) {
 	if (length(cols)!=length(rs)) stop("rs and cols must have the same number of elements.")
 	if (verbose) cat("Calculating enrichment\n")
 	rs.enrich <- lapply(rs, enrichmentCalc, organism, seqLen)
@@ -28,10 +28,10 @@ setMethod("enrichmentPlot", c("GRangesList", "BSgenome"), function(rs, organism,
 		for (i in 1:length(rs)) rs.enrich[[i]]$coverage <- rs.enrich[[i]]$coverage/(rs.counts[[i]]/1000000)
 	}
 	plot(x=rs.enrich[[1]]$coverage, y=rs.enrich[[1]]$bases, type="l", col=cols[1], xlim=xlim, main=main, ylab="Frequency", log="y",
-			xlab=if (total.lib.size) "Normalised Enrichment Level of reads" else "Enrichment Level of reads")
+			xlab=if (total.lib.size) "Normalised Enrichment Level of reads" else "Enrichment Level of reads", ...)
 	if (length(rs)>1) for (i in 2:length(rs)) {
-		lines(x=rs.enrich[[i]]$coverage, y=rs.enrich[[i]]$bases, col=cols[i])
+		lines(x=rs.enrich[[i]]$coverage, y=rs.enrich[[i]]$bases, col=cols[i], ...)
 	}
-	legend("topright", lty=1, col=cols, legend=names(rs))
+	legend("topright", lty=1, col=cols, legend=names(rs), ...)
 	invisible(rs.enrich)
 })
