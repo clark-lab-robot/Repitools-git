d7a3ddfb1cf7dee508557746fdedd078c8a87683
dario <dario@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-12-02 22:47:28 +0000

    Clustered heatmap code split into 2 functions - getTSScoverage and clusteredHeatmap
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/repitools@84 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Repitools/R/clusteredHeatmap.R
+++ pkg/Repitools/R/clusteredHeatmap.R
@@ -1,81 +1,18 @@
-setGeneric("clusteredHeatmap", signature = "readsIPs", function(readsIPs, ...){standardGeneric("clusteredHeatmap")})
+setGeneric("clusteredHeatmap", signature = "coverageList", function(coverageList, ...){standardGeneric("clusteredHeatmap")})
 
-setMethod("clusteredHeatmap", "GRangesList", function(readsIPs, exptTypes, geneAnno, upPct = 50, downPct = 100, cvgResolution = 10, smoothingWidth = 2001, capQuantile = 0.95, nClusters = 5, expr, cols = colorpanel(64, "blue", "white", "red"), tName = "Clustered Enrichment")
+setMethod("clusteredHeatmap", "list", function(coverageList, geneAnno, capQuantile = 0.95, nClusters = 5, expr, cols = colorpanel(64, "blue", "white", "red"), logWidth = TRUE, tName = "Clustered Enrichment")
 {
-	# Pool any replicates found.
-	typeCounts <- table(exptTypes)
-	if(any(typeCounts > 1))
-	{
-		oldOrder <- unique(exptTypes)
-		repTypes <- names(typeCounts)[typeCounts > 1]
-		pooledIPs <- lapply(repTypes, function(repType)
-		{
-			whichReps <- which(exptTypes == repType)
-			do.call(c, unname(as.list(readsIPs[whichReps])))
-		})
-		names(pooledIPs) <- repTypes
-		uniqueIdxs <- exptTypes %in% names(typeCounts)[typeCounts == 1]
-		uniqueIPs <- as.list(readsIPs[uniqueIdxs])
-		names(uniqueIPs) <- exptTypes[uniqueIdxs]
-		readsIPs <- c(pooledIPs, uniqueIPs)
-		readsIPs <- GRangesList(readsIPs[oldOrder])
-	}
-
-	# Scale all coverages to be as if there were 10 million reads in each GRanges object.
-	coverageIPs <- lapply(readsIPs, function(readsIP) coverage(readsIP) * 10000000 / length(readsIP))
-
-	pctPositions <- seq(-upPct, downPct, cvgResolution)
 	widths <- geneAnno$end - geneAnno$start + 1
-	geneAnno$tss <- ifelse(geneAnno$strand == '+', geneAnno$start, geneAnno$end)
-	
-	flank = floor(smoothingWidth / 2)
-	# Get matrix for each IP mark.
-	coverageMatrices <- lapply(coverageIPs, function(coverageIP)
-	{
-		TSScoverageList <- list()
-		for(geneIndex in 1:nrow(geneAnno))
-		{
-			# Get smoothing region. Extra flanks so that we can smooth the edge of the region nicely.
-			if(geneAnno[geneIndex, "strand"] == '+')
-			{
-				region <- round(geneAnno[geneIndex, "start"] - upPct / 100 * widths[geneIndex] - flank) : round(geneAnno[geneIndex, "start"] + (downPct / 100 * widths[geneIndex]) + flank)
-				coverageGene <- tryCatch(coverageIP[[geneAnno[geneIndex, "chr"]]][region], error = function(errMsg) "Error getting all bases' coverage.")
-			} else {
-				region <- round(geneAnno[geneIndex, "end"] - downPct / 100 * widths[geneIndex] - flank) : round(geneAnno[geneIndex, "end"] + (upPct / 100 * widths[geneIndex]) + flank)
-				coverageGene <- tryCatch(rev(coverageIP[[geneAnno[geneIndex, "chr"]]][region]), error = function(errMsg) "Error getting all bases' coverage.")
-			}
-
-			# If could get all bases, then smooth.
-			if(class(coverageGene) == "Rle") {coverageGene <- runmean(coverageGene, smoothingWidth, endrule = "constant")} else {TSScoverageList[[geneIndex]] <- "Error"; next;}
-
-			# Get positions of precentage distances, then get the coverage at that point.
-			samplingPositions <- round(flank + (0:(length(pctPositions)-1) / (length(pctPositions)-1)) * (length(region) - 2 * flank)) + 1
-			reprCoverage <- as.numeric(coverageGene[samplingPositions])
-			names(reprCoverage) <- pctPositions
-			TSScoverageList[[geneIndex]] <- reprCoverage	
-		}
-		names(TSScoverageList) <- 1:nrow(geneAnno)	
-	
-		# Find which gene didn't have all coverage available. Eliminate from the coverage list.
-		whichEliminate <- which(sapply(TSScoverageList, function(geneCoverage) length(geneCoverage) == 1))
-		if(length(whichEliminate) > 0)
-		{
-			TSScoverageList <- TSScoverageList[-whichEliminate]
-		}
-		TSScoverageTable <- do.call(rbind, TSScoverageList)
-		rownames(TSScoverageTable) <- names(TSScoverageList)
-		return(TSScoverageTable)
-	})
 
 	# Now only keep in the expression and width vectors, the genes with all coverage sample positions.
-	expr <- expr[as.integer(rownames(coverageMatrices[[1]]))]		
-	widths <- widths[as.integer(rownames(coverageMatrices[[1]]))]
-	geneAnno <- geneAnno[as.integer(rownames(coverageMatrices[[1]])), ]
+	expr <- expr[as.integer(rownames(coverageList[[1]]))]		
+	widths <- widths[as.integer(rownames(coverageList[[1]]))]
+	geneAnno <- geneAnno[as.integer(rownames(coverageList[[1]])), ]
 
 	# Find the maximum score allowable, then make any scores bigger be the maximum score.
-	allScores <- do.call(cbind, coverageMatrices)
+	allScores <- do.call(cbind, coverageList)
 	maxIntensity = quantile(allScores, capQuantile)
-	coverageMatrices <- lapply(coverageMatrices, function(coverageMatrix)
+	coverageList <- lapply(coverageList, function(coverageMatrix)
 	{
 		coverageMatrix[coverageMatrix > maxIntensity] = maxIntensity
 		return(coverageMatrix)
@@ -98,12 +35,12 @@ setMethod("clusteredHeatmap", "GRangesList", function(readsIPs, exptTypes, geneA
 	}))
 
 	# Re-arrange the ChIP and expression data and vector of gene widths.
-	coverageMatrices <- lapply(coverageMatrices, function(coverageMatrix) coverageMatrix[sortedIndices, ])
+	coverageList <- lapply(coverageList, function(coverageMatrix) coverageMatrix[sortedIndices, ])
 	expr <- expr[sortedIndices]
 	widths <- widths[sortedIndices]
 
 	# Plot heatmap.
-	layout(rbind(1:(length(coverageMatrices) + 3)), widths=c(1, rep(3, length(coverageMatrices)), 1, 1))
+	layout(rbind(1:(length(coverageList) + 3)), widths=c(1, rep(3, length(coverageList)), 1, 1))
 	par(mai=c(1.02,0.50,0.82,0.05))
   	
 	par(oma = c(0, 0, 0, 0))
@@ -112,16 +49,20 @@ setMethod("clusteredHeatmap", "GRangesList", function(readsIPs, exptTypes, geneA
 	image(y=seq(1/nBins/2, 1-(1/nBins/2), 1/nBins), z=rbind(1:nBins), col = cols, axes = FALSE, xlab = "Signal Intensity", ylab = NA)
 	axis(2, at=(0:nBins)/nBins, labels=format(seq(heatmapRange[1], heatmapRange[2], length.out = nBins + 1), digits = 2))
 
+	posLabels <- colnames(coverageList[[1]])
+	positions <- as.integer(gsub('%', '', posLabels)) # Find if sampled at percentages or bases.
 	par(mai=c(1.02,0.05,0.82,0.05))
 	mapply(function(coverageMatrix, exptName)
 	{
-		image(pctPositions, 1:nrow(geneAnno), t(coverageMatrix), xlab="Position (%) relative to TSS", yaxt = "n", ylab = "Gene", col = cols, main = exptName)
-		
+		image(positions, 1:nrow(geneAnno), t(coverageMatrix), xlab="Position relative to TSS", xaxt = "n", yaxt = "n", ylab = "Gene", col = cols, main = exptName)
+		axis(1, positions, labels = posLabels)		
+
 		# Add lines delimiting the cluster boundaries.
 		boundaries <- cumsum(table(clusterID)[exprOrder])[-nClusters]
 		abline(h = boundaries, lwd = 2)
-	}, coverageMatrices, unique(exptTypes))
+	}, coverageList, names(coverageList))
 
+	if(logWidth == TRUE) widths <- log(widths)
 	par(mai=c(1.02,0.05,0.82,0.50))
 	plot(expr, y = 1:length(expr), yaxs = 'i', pch = 19, xlab = "log2 expression", ylab = NA, yaxt = "n", cex = 0.5)
 	plot(widths, y = 1:length(widths), yaxs = 'i', pch = 19, xlab = "Gene widths", ylab = NA, yaxt = "n", cex = 0.5)	
---
+++ pkg/Repitools/R/getTSScoverage.R
@@ -0,0 +1,90 @@
+setGeneric("getTSScoverage", signature = "readsIPs", function(readsIPs, ...){standardGeneric("getTSScoverage")})
+
+setMethod("getTSScoverage", "GRangesList", function(readsIPs, exptTypes, geneAnno, up = 50, down = 100, distType = c("percent", "base"), cvgResolution = 10, smoothingWidth = 2001)
+{
+	distType <- match.arg(distType)
+
+	# Pool any replicates
+	readsIPs <- split(unlist(readsIPs), rep(exptTypes, elementLengths(readsIPs)))
+
+	positions <- seq(-up, down, cvgResolution)
+	if(distType == "percent")
+		positionsLabels <- paste(positions, '%', sep = '')
+	else
+		positionsLabels <- positions
+	widths <- geneAnno$end - geneAnno$start + 1
+	geneAnno$tss <- ifelse(geneAnno$strand == '+', geneAnno$start, geneAnno$end)
+
+	# Get base positions to extract in 5' to 3' direction for every gene.
+	regionBasesList <- lapply(1:nrow(geneAnno), function(geneIndex)
+	{
+		if(geneAnno[geneIndex, "strand"] == '+')
+		{
+			if(distType == "percent")
+				region <- c(round(geneAnno[geneIndex, "start"] - up / 100 * widths[geneIndex] - flank), round(geneAnno[geneIndex, "start"] + (down / 100 * widths[geneIndex]) + flank))
+			else
+				region <- c(round(geneAnno[geneIndex, "start"] - up - flank), round(geneAnno[geneIndex, "start"] + down + flank))
+		} else {
+			if(distType == "percent")
+				region <- c(round(geneAnno[geneIndex, "end"] + (up / 100 * widths[geneIndex]) + flank), round(geneAnno[geneIndex, "end"] - down / 100 * widths[geneIndex] - flank))
+			else
+				region <- c(round(geneAnno[geneIndex, "end"] + up + flank), round(geneAnno[geneIndex, "end"] - down - flank))
+		}
+		return(region)
+	})
+
+	# Get sampling positions in the extracted region for every gene.
+	if(distType == "percent")
+	{
+		samplingPositions <- lapply(1:nrow(geneAnno), function(geneIndex)
+		{
+			round(flank + (0:(length(positions)-1) / (length(positions)-1)) * widths[geneIndex]) + 1
+		})
+	} else {
+		samplingPositions <- round(flank + (0:(length(positions)-1) * cvgResolution)) + 1
+	}
+
+	flank = floor(smoothingWidth / 2)
+
+	# Get coverage sampling matrix for each IP mark.
+	coverageMatrices <- lapply(readsIPs, function(readsIP)
+	{
+		# Scale all coverages to be as if there were 10 million reads in each GRanges object.
+		# Make the coverage vectors be numeric. Much faster to access than Rle.
+		coverageIP <- coverage(readsIP) * 10000000 / length(readsIP)
+		
+		TSScoverageList <- list()
+		for(geneIndex in 1:nrow(geneAnno))
+		{
+			# Get smoothing region. Extra flanks so that we can smooth the edge of the region nicely.
+			coverageGene <- tryCatch(coverageIP[[geneAnno[geneIndex, "chr"]]][regionBasesList[[geneIndex]][1]:regionBasesList[[geneIndex]][2]], error = function(errMsg) "Error getting all bases' coverage.")
+
+			# If could get all bases, then smooth.
+			if(class(coverageGene) == "Rle") {coverageGene <- runmean(coverageGene, smoothingWidth, endrule = "constant")} else {TSScoverageList[[geneIndex]] <- "Error"; next;}
+
+			# Get the coverage at the sampling positions.
+			if(distType == "percent")
+				reprCoverage <- as.numeric(coverageGene[samplingPositions[[geneIndex]]])
+			else
+				reprCoverage <- as.numeric(coverageGene[samplingPositions])
+
+			names(reprCoverage) <- positionsLabels
+			TSScoverageList[[geneIndex]] <- reprCoverage	
+		}
+		names(TSScoverageList) <- 1:nrow(geneAnno)
+	
+		# Find which genes didn't have all coverage available. Eliminate from the coverage list.
+		whichEliminate <- which(sapply(TSScoverageList, function(geneCoverage) length(geneCoverage) == 1))
+		if(length(whichEliminate) > 0)
+		{
+			TSScoverageList <- TSScoverageList[-whichEliminate]
+		}
+		TSScoverageTable <- do.call(rbind, TSScoverageList)
+		rownames(TSScoverageTable) <- names(TSScoverageList)
+		return(TSScoverageTable)
+	})
+
+	rm(readsIPs, coverageIPs)
+	gc()
+	return(coverageMatrices)
+})
