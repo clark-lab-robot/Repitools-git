7226ad9632b15d5226a44bff3835acdf0ae4bafa
markrobinsonca <markrobinsonca@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-12-04 08:41:31 +0000

    update to getTSScoverage()
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/repitools@91 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Repitools/R/getTSScoverage.R
+++ pkg/Repitools/R/getTSScoverage.R
@@ -6,7 +6,7 @@ setMethod("getTSScoverage", "GRangesList", function(readsIPs, exptTypes, geneAnn
 
 	# Pool any replicates
 	#readsIPs <- split(unlist(readsIPs), rep(exptTypes, elementLengths(readsIPs)))
-  inds <- split(1:length(exptTypes), unique(exptTypes))
+  inds <- split(1:length(exptTypes), exptTypes)
   
   readsIPs <- lapply(inds, FUN=function(u) {
     gr <- GRanges()
