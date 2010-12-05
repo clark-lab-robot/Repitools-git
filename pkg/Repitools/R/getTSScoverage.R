87e800dd37a8a4289c1c4a0c427b9f4cc83471fb
dario <dario@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-12-05 23:22:02 +0000

    getTSScoverage now has option to extend short reads to fixed length. Also some progress printing added and loops made faster.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/repitools@92 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Repitools/R/getTSScoverage.R
+++ pkg/Repitools/R/getTSScoverage.R
@@ -1,24 +1,30 @@
 setGeneric("getTSScoverage", signature = "readsIPs", function(readsIPs, ...){standardGeneric("getTSScoverage")})
 
-setMethod("getTSScoverage", "GRangesList", function(readsIPs, exptTypes, geneAnno, up = 50, down = 100, distType = c("percent", "base"), cvgResolution = 10, smoothingWidth = 2001)
+setMethod("getTSScoverage", "GRangesList", function(readsIPs, seqLen = 300, exptTypes, geneAnno, up = 50, down = 100, distType = c("percent", "base"), cvgResolution = 10, smoothingWidth = 2001)
 {
 	distType <- match.arg(distType)
+	readsIPs <- endoapply(readsIPs, resize, seqLen)
 
-	# Pool any replicates
-	#readsIPs <- split(unlist(readsIPs), rep(exptTypes, elementLengths(readsIPs)))
-  inds <- split(1:length(exptTypes), exptTypes)
-  
-  readsIPs <- lapply(inds, FUN=function(u) {
-    gr <- GRanges()
-    cat(":")
-    for(i in 1:length(u)) {
-      cat(".")
-      gr <- c(gr,readsIPs[[ u[i] ]])
-    }
-    return(gr)
-  })
-  gc()
-  cat("\n")
+	# Pool any replicates.
+	cat("Pooling any replicates.\n")
+	typesInds <- split(1:length(exptTypes), exptTypes)
+
+	readsIPs <- mapply(function(typeInds, typeName)
+	{
+		cat("Processing ", typeName, '\n')
+		if(length(typeInds) == 1)
+		{
+			readsIPs[[typeInds]]
+		} else {
+			pooledReads <- GRanges()
+			for(replicateIndex in 1:length(typeInds))
+			{
+				pooledReads <- c(pooledReads, readsIPs[[typeInds[replicateIndex]]])
+			}
+			return(pooledReads)
+		}
+	}, typesInds, names(typesInds))
+  	gc()
 
 	positions <- seq(-up, down, cvgResolution)
 	if(distType == "percent")
@@ -29,37 +35,37 @@ setMethod("getTSScoverage", "GRangesList", function(readsIPs, exptTypes, geneAnn
 	geneAnno$tss <- ifelse(geneAnno$strand == '+', geneAnno$start, geneAnno$end)
 	flank = floor(smoothingWidth / 2)
   
-  cat("Getting positions.")
+	cat("Getting positions of regions to extract.\n")
 	# Get base positions to extract in 5' to 3' direction for every gene.
-	regionBasesList <- lapply(1:nrow(geneAnno), function(geneIndex)
+	regionBasesList <- mapply(function(start, end, strand, width)
 	{
-		if(geneAnno[geneIndex, "strand"] == '+')
+		if(strand == '+')
 		{
 			if(distType == "percent")
-				region <- c(round(geneAnno[geneIndex, "start"] - up / 100 * widths[geneIndex] - flank), round(geneAnno[geneIndex, "start"] + (down / 100 * widths[geneIndex]) + flank))
+				region <- c(round(start - up / 100 * width - flank), round(start + (down / 100 * width) + flank))
 			else
-				region <- c(round(geneAnno[geneIndex, "start"] - up - flank), round(geneAnno[geneIndex, "start"] + down + flank))
+				region <- c(round(start - up - flank), round(start + down + flank))
 		} else {
 			if(distType == "percent")
-				region <- c(round(geneAnno[geneIndex, "end"] + (up / 100 * widths[geneIndex]) + flank), round(geneAnno[geneIndex, "end"] - down / 100 * widths[geneIndex] - flank))
+				region <- c(round(end + up / 100 * width + flank), round(end - down / 100 * width - flank))
 			else
-				region <- c(round(geneAnno[geneIndex, "end"] + up + flank), round(geneAnno[geneIndex, "end"] - down - flank))
+				region <- c(round(end + up + flank), round(end - down - flank))
 		}
 		return(region)
-	})
+	}, geneAnno$start, geneAnno$end, geneAnno$strand, widths, SIMPLIFY = FALSE)
 
 	# Get sampling positions in the extracted region for every gene.
 	if(distType == "percent")
 	{
-		samplingPositions <- lapply(1:nrow(geneAnno), function(geneIndex)
+		samplingPositions <- lapply(widths, function(width)
 		{
-			round(flank + (0:(length(positions)-1) / (length(positions)-1)) * widths[geneIndex]) + 1
+			round(flank + (0:(length(positions)-1) / (length(positions)-1)) * width) + 1
 		})
 	} else {
 		samplingPositions <- round(flank + (0:(length(positions)-1) * cvgResolution)) + 1
 	}
 
-  cat("Getting coverage matrices.")
+	cat("Getting coverage matrices.\n")
 	# Get coverage sampling matrix for each IP mark.
 	coverageMatrices <- lapply(readsIPs, function(readsIP)
 	{
@@ -67,14 +73,13 @@ setMethod("getTSScoverage", "GRangesList", function(readsIPs, exptTypes, geneAnn
 		# Make the coverage vectors be numeric. Much faster to access than Rle.
 		coverageIP <- coverage(readsIP) * 10000000 / length(readsIP)
 		
-		TSScoverageList <- list()
-		for(geneIndex in 1:nrow(geneAnno))
+		TSScoverageList <- mapply(function(chr, regionBases, geneIndex)
 		{
 			# Get smoothing region. Extra flanks so that we can smooth the edge of the region nicely.
-			coverageGene <- tryCatch(coverageIP[[geneAnno[geneIndex, "chr"]]][regionBasesList[[geneIndex]][1]:regionBasesList[[geneIndex]][2]], error = function(errMsg) "Error getting all bases' coverage.")
+			coverageGene <- tryCatch(coverageIP[[chr]][regionBases[1]:regionBases[2]], error = function(errMsg) "Error getting all bases' coverage.")
 
 			# If could get all bases, then smooth.
-			if(class(coverageGene) == "Rle") {coverageGene <- runmean(coverageGene, smoothingWidth, endrule = "constant")} else {TSScoverageList[[geneIndex]] <- "Error"; next;}
+			if(class(coverageGene) == "Rle") {coverageGene <- runmean(coverageGene, smoothingWidth, endrule = "constant")} else {return("Error")}
 
 			# Get the coverage at the sampling positions.
 			if(distType == "percent")
@@ -83,8 +88,8 @@ setMethod("getTSScoverage", "GRangesList", function(readsIPs, exptTypes, geneAnn
 				reprCoverage <- as.numeric(coverageGene[samplingPositions])
 
 			names(reprCoverage) <- positionsLabels
-			TSScoverageList[[geneIndex]] <- reprCoverage	
-		}
+			return(reprCoverage)	
+		}, geneAnno$chr, regionBasesList, 1:nrow(geneAnno), SIMPLIFY = FALSE)
 		names(TSScoverageList) <- 1:nrow(geneAnno)
 	
 		# Find which genes didn't have all coverage available. Eliminate from the coverage list.
@@ -98,7 +103,7 @@ setMethod("getTSScoverage", "GRangesList", function(readsIPs, exptTypes, geneAnn
 		return(TSScoverageTable)
 	})
 
-	rm(readsIPs, coverageIPs)
+	rm(readsIPs)
 	gc()
 	return(coverageMatrices)
 })
