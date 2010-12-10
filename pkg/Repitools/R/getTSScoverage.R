b812f1262aaaed2574afa62dc4390e2dbaa6fde3
dario <dario@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-12-10 05:14:55 +0000

    Views used to extract coverage data in getTSScoverage now.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/repitools@97 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Repitools/R/getTSScoverage.R
+++ pkg/Repitools/R/getTSScoverage.R
@@ -4,6 +4,7 @@ setMethod("getTSScoverage", "GRangesList", function(readsIPs, seqLen = 300, expt
 {
 	distType <- match.arg(distType)
 	readsIPs <- endoapply(readsIPs, resize, seqLen)
+	chrLengths <- seqlengths(readsIPs)
 
 	# Pool any replicates.
 	cat("Pooling any replicates.\n")
@@ -26,18 +27,24 @@ setMethod("getTSScoverage", "GRangesList", function(readsIPs, seqLen = 300, expt
 	}, typesInds, names(typesInds))
   	gc()
 
+	# Prepare for extracting Views by sorting in chromosomal order.
+	
+	geneAnno$original <- 1:nrow(geneAnno)
+	chrOrder <- order(geneAnno$chr)
+	geneAnno <- geneAnno[chrOrder, ]
+	annoGR <- GRanges(geneAnno$chr, IRanges(geneAnno$start, geneAnno$end), geneAnno$strand, order = geneAnno$original)
+
 	positions <- seq(-up, down, cvgResolution)
 	if(distType == "percent")
 		positionsLabels <- paste(positions, '%', sep = '')
 	else
 		positionsLabels <- positions
 	widths <- geneAnno$end - geneAnno$start + 1
-	geneAnno$tss <- ifelse(geneAnno$strand == '+', geneAnno$start, geneAnno$end)
 	flank = floor(smoothingWidth / 2)
   
 	cat("Getting positions of regions to extract.\n")
-	# Get base positions to extract in 5' to 3' direction for every gene.
-	regionBasesList <- mapply(function(start, end, strand, width)
+	# Get base positions to extract for every gene.
+	regionBasesList <- mapply(function(chr, start, end, strand, width)
 	{
 		if(strand == '+')
 		{
@@ -47,12 +54,27 @@ setMethod("getTSScoverage", "GRangesList", function(readsIPs, seqLen = 300, expt
 				region <- c(round(start - up - flank), round(start + down + flank))
 		} else {
 			if(distType == "percent")
-				region <- c(round(end + up / 100 * width + flank), round(end - down / 100 * width - flank))
+				region <- c(round(end - down / 100 * width - flank), round(end + up / 100 * width + flank))
 			else
-				region <- c(round(end + up + flank), round(end - down - flank))
+				region <- c(round(end - down - flank), round(end + up + flank))
 		}
-		return(region)
-	}, geneAnno$start, geneAnno$end, geneAnno$strand, widths, SIMPLIFY = FALSE)
+
+		if(region[1] < 0 || region[2] > chrLengths[[chr]]) # Region overlaps past edge of chromosome.
+			return(numeric())
+		else
+			return(region)
+	}, geneAnno$chr, geneAnno$start, geneAnno$end, geneAnno$strand, widths, SIMPLIFY = FALSE)
+
+	# Eliminate regions past edges.
+	whichOutside <- which(sapply(regionBasesList, function(regionBases) length(regionBases) == 0))
+	annoGR <- annoGR[-whichOutside, ]
+	widths <- widths[-whichOutside]
+	regionBasesList <- regionBasesList[-whichOutside]
+	whichReverse <- which(strand(annoGR) == '-')
+
+	# Create windows to use in Views().
+	windowsGR <- GRanges(as.character(seqnames(annoGR)), IRanges(sapply(regionBasesList, function(bases) bases[1]), sapply(regionBasesList, function(bases) bases[2])))
+	windowsRL <- as(windowsGR, "RangesList")
 
 	# Get sampling positions in the extracted region for every gene.
 	if(distType == "percent")
@@ -62,7 +84,7 @@ setMethod("getTSScoverage", "GRangesList", function(readsIPs, seqLen = 300, expt
 			round(flank + (0:(length(positions)-1) / (length(positions)-1)) * width) + 1
 		})
 	} else {
-		samplingPositions <- round(flank + (0:(length(positions)-1) * cvgResolution)) + 1
+		samplingPositions <- rep(list(round(flank + (0:(length(positions)-1) * cvgResolution)) + 1), length(windowsGR))
 	}
 
 	cat("Getting coverage matrices.\n")
@@ -70,36 +92,20 @@ setMethod("getTSScoverage", "GRangesList", function(readsIPs, seqLen = 300, expt
 	coverageMatrices <- lapply(readsIPs, function(readsIP)
 	{
 		# Scale all coverages to be as if there were 10 million reads in each GRanges object.
-		# Make the coverage vectors be numeric. Much faster to access than Rle.
 		coverageIP <- coverage(readsIP) * 10000000 / length(readsIP)
-		
-		TSScoverageList <- mapply(function(chr, regionBases, geneIndex)
-		{
-			# Get smoothing region. Extra flanks so that we can smooth the edge of the region nicely.
-			coverageGene <- tryCatch(coverageIP[[chr]][regionBases[1]:regionBases[2]], error = function(errMsg) "Error getting all bases' coverage.")
+		coverageIP <- coverageIP[names(windowsRL)]
 
-			# If could get all bases, then smooth.
-			if(class(coverageGene) == "Rle") {coverageGene <- runmean(coverageGene, smoothingWidth, endrule = "constant")} else {return("Error")}
+		# Get smoothing region. Extra flanks so that we can smooth the edge of the region nicely.
+		coverageGenes <- viewApply(unlist(Views(coverageIP, windowsRL)), function(aView) Rle(as.integer(aView)))
+		coverageGenes[whichReverse] <- lapply(coverageGenes[whichReverse], rev)
 
-			# Get the coverage at the sampling positions.
-			if(distType == "percent")
-				reprCoverage <- as.numeric(coverageGene[samplingPositions[[geneIndex]]])
-			else
-				reprCoverage <- as.numeric(coverageGene[samplingPositions])
+		# Running mean smoothing.
+		coverageGenes <- RleList(lapply(coverageGenes, function(coverageGene) runmean(coverageGene, smoothingWidth, endrule = "constant")))
 
-			names(reprCoverage) <- positionsLabels
-			return(reprCoverage)	
-		}, geneAnno$chr, regionBasesList, 1:nrow(geneAnno), SIMPLIFY = FALSE)
-		names(TSScoverageList) <- 1:nrow(geneAnno)
-	
-		# Find which genes didn't have all coverage available. Eliminate from the coverage list.
-		whichEliminate <- which(sapply(TSScoverageList, function(geneCoverage) length(geneCoverage) == 1))
-		if(length(whichEliminate) > 0)
-		{
-			TSScoverageList <- TSScoverageList[-whichEliminate]
-		}
-		TSScoverageTable <- do.call(rbind, TSScoverageList)
-		rownames(TSScoverageTable) <- names(TSScoverageList)
+		# Get the coverage at the sampling positions.
+		TSScoverageTable=do.call(rbind, viewApply(Views(coverageGenes, RangesList(lapply(samplingPositions, function(x) IRanges(x, width = 1)))), as.numeric)@listData)
+		colnames(TSScoverageTable) <- positionsLabels	
+		rownames(TSScoverageTable) <- elementMetadata(annoGR)[, "order"]
 		return(TSScoverageTable)
 	})
 
