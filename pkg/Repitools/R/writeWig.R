81a115b1569251fd8b39b0af5e57519cc618ef0a
Aaron Statham <aaron.l.statham@gmail.com>
2011-03-28 20:28:57 -0700

    Removed writeWig copy pasting

--- pkg/Repitools/R/writeWig.R
+++ pkg/Repitools/R/writeWig.R
@@ -1,52 +1,12 @@
 setOldClass("AffymetrixCelSet")
 
-setGeneric("writeWig", function(rs, ...){standardGeneric("writeWig")})
+setGeneric("writeWig", function(rs, ...) standardGeneric("writeWig"))
 
-setMethod("writeWig", "GenomeDataList", function(rs, seqLen, design=NULL, sample=20, dropZero=TRUE, normalise=TRUE, verbose=TRUE, ...) {
-	scipen <- getOption("scipen")
-	options(scipen=100)
-	if (verbose) cat("Extending the reads by ", seqLen,"bp\n", sep="" )
-	rs.ext <- extendReads(rs, seqLen)
-	if (verbose) cat("Creating coverage objects\n")
-	rs.cov <- gdapply(rs.ext, coverage)
-	if (normalise) {
-		if (verbose) cat("Calculating normalisation counts\n")
-		norm <- laneCounts(rs)/1000000
-	}
-	if (is.null(design)) {
-		if (verbose) cat("Default design matrix\n")
-		design <- matrix(0, nrow=length(rs), ncol=length(rs), dimnames=list(NULL, paste(names(rs), ".wig.gz", sep="")))
-		for (i in 1:length(rs)) design[i,i] <- 1
-	}
-	for (i in 1:ncol(design)) {
-		filename <- colnames(design)[i]
-		if (verbose) cat("Processing column",i,"of design matrix -",filename,"\n")
-		f1 <- if (grepl("gz$",filename)) gzfile(filename, open="wt") else file(filename, open="wt")
-
-		thisDes <- which(design[,i]!=0)
-		thisMul <- design[thisDes,i]
-		rs.this <- rs.cov[thisDes]		
-		for (j in 1:length(rs.this[[1]])) {
-			if (verbose) cat(names(rs.this[[1]])[j]," ")
-			#Write header for this chromosome
-			writeLines(paste("variableStep chrom=", names(rs.this[[1]])[j]," span=", sample, sep=""), f1)
-			#ensure all lanes being used have the same length
-			chrMax <- max(sapply(rs.this, function(x) length(x[[j]])))
-			for (k in 1:length(rs.this)) rs.this[[k]][[j]] <- c(rs.this[[k]][[j]], Rle(0, chrMax-length(rs.this[[k]][[j]]))) 
-			bp <- 1:(length(rs.this[[1]][[j]]) %/% sample)*sample
-			bp.score <- as.integer(rs.this[[1]][[j]][bp])/norm[thisDes[1]]*thisMul[1]
-			if (length(thisDes)>1) for (k in 2:length(thisDes)) bp.score <- bp.score + as.integer(rs.this[[k]][[j]][bp])/norm[thisDes[k]]*thisMul[k]
-			temp <- if (dropZero) paste(bp[bp.score!=0], bp.score[bp.score!=0], sep="\t")
-				else paste(bp, bp.score, sep="\t")
-			writeLines(temp, f1)
-		}
-		if (verbose) cat("\n")
-		close(f1)
-	}
-	options(scipen=scipen)
+setMethod("writeWig", "GenomeDataList", function(rs, ...) {
+    writeWig(.GDL2GRL(rs), ...)
 })
 
-setMethod("writeWig", "GRangesList", function(rs, seqLen, design=NULL, sample=20, dropZero=TRUE, normalise=TRUE, verbose=TRUE, ...) {
+setMethod("writeWig", "GRangesList", function(rs, seqLen, design=NULL, sample=20, dropZero=TRUE, normalise=TRUE, verbose=TRUE) {
 	require(GenomicRanges)	
 
 	scipen <- getOption("scipen")
@@ -92,7 +52,7 @@ setMethod("writeWig", "GRangesList", function(rs, seqLen, design=NULL, sample=20
 	options(scipen=scipen)
 })
 
-setMethod("writeWig", "AffymetrixCelSet", function(rs, design=NULL, log2adjust=TRUE, verbose=TRUE, ...) {
+setMethod("writeWig", "AffymetrixCelSet", function(rs, design=NULL, log2adjust=TRUE, verbose=TRUE) {
 	scipen <- getOption("scipen")
 	options(scipen=100)
 	if (is.null(design)) {
